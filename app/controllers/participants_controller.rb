77aa5476e2165bb34f8efe34c40828dd58ca67eb
mydearcodes <gorselsanat@gmail.com>
2021-07-11 14:01:56 -0700

    have set current user permissions and redirections

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,27 +1,33 @@
 class ParticipantsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_participant, only: %i[ show edit update destroy ]
+  before_action :check_participant, only: [:show, :index]
+  before_action :check_owner, only: [:show, :update, :destroy]
 
   # GET /participants or /participants.json
   def index
     if current_user.admin?
-    @participants = Participant.all
+    @participants = Participant.all    
     else
-      if current_user.participants.exists?
-     redirect_to @current_user.participant 
-      else
-        redirect_to new
-      end
+      redirect_to new_participant_url
     end
+    
+    
   end
 
   # GET /participants/1 or /participants/1.json
   def show
+    
+    
   end
 
   # GET /participants/new
   def new
+    if current_user.participant.nil?
     @participant = Participant.new
+    else
+      redirect_to @current_user.participant
+    end
   end
 
   # GET /participants/1/edit
@@ -30,8 +36,9 @@ class ParticipantsController < ApplicationController
 
   # POST /participants or /participants.json
   def create
-    @participant = Participant.new(participant_params)
-
+    
+    @participant = current_user.build_participant(participant_params)
+    
     respond_to do |format|
       if @participant.save
         format.html { redirect_to @participant, notice: "Participant was successfully created." }
@@ -75,4 +82,18 @@ class ParticipantsController < ApplicationController
     def participant_params
       params.require(:participant).permit(:firstname, :familyname, :kyc_approved, :agreement, :privacy_agr, :eth_adress, :cont_amount, :eth_tx, :user_id)
     end
+
+
+    def check_participant
+      if current_user.participant.nil?
+        redirect_to new_participant_url
+      end
+    end
+
+    def check_owner
+      if @participant.user != current_user && current_user.admin == false
+        redirect_to current_user.participant
+      end
+    end
+
 end
